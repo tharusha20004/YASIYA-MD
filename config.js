cc3f5a6f7936c151f8e96f1cd6f68c87082df4f2
tharusha20004 <tharushasuwahas@gmail.com>
2025-11-13 07:20:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~dEYQGYSS#qs1M8vE-TVDNkgf3nGvyiY4Tv9qtnt-L1M4nAAkNpPc'
 };
