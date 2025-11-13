15109d8c66e975fe3081dc3fb49791db5cf06681
tharusha20004 <tharushasuwahas@gmail.com>
2025-11-13 10:02:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~file/oEZCQCgI#vaxxm1XFf2hucqy9tdJx6YbieO3Ykoh7o8apOtaGEx4'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~file/dFhHXSRJ#6w9-URO4ZtCDtxufnhieHNiI75McUr6VsJSa2yzi5ts'
 };
