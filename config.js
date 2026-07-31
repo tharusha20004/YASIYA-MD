3ecd42b99a9c255519a1696df66c149d1d659b12
tharusha20004 <tharushasuwahas@gmail.com>
2026-07-31 18:40:00 +0530

    Update default SESSION_ID in config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'Enter Your Session ID.',
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?1GxGbP.',
     PORT: process.env.PORT || 8000,
     SESSION_NAME: process.env.SESSION_NAME || "auth_info_baileys"
 };
